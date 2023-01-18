b0bbe085bcc89d44fea29ee9777d378debffee05
Akilah Powell <atpjun@gmail.com>
2023-01-18 15:14:16 -0800

    created the filter, not working yet, but created the function

--- src/useCountries.js
+++ src/useCountries.js
@@ -4,12 +4,16 @@ import { getCountries } from './services/countries';
 
 function useCountries() {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
   
 
   useEffect (() => {
     const fetchData = async () => {
       const data = await getCountries();
       setCountries(data);
+      setLoading(false);
+
     };
     fetchData();
   }, 
@@ -18,8 +22,12 @@ function useCountries() {
 
 function filterCountries() {
   return countries.filter((country) => {
-    return country.name.includes(query) && (country.continent === continent || continent 'All')
-  
+    return (
+      country.name.includes(query) && (country.continent === continent || continent === 'All')
+    );
   });
-
 }
+
+<div className="countries">
+  <h1>Countries of the World</h1>
+</div>
