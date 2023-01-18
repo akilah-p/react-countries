6de9e2e76beac61c1ba6d0c6b3bf5f8334e1fc22
Akilah Powell <atpjun@gmail.com>
2023-01-18 15:54:30 -0800

    got everything inside the function

--- src/hooks/getThings.js
+++ src/hooks/getThings.js
@@ -18,44 +18,45 @@ function useCountries() {
 
     };
     fetchData();
-  }, 
-  []);
-}
+  }, []);
 
-function filterCountries() {
-  return countries.filter((country) => {
-    return (
-      country.name.includes(query) && (country.continent === continent || continent === 'All')
-    );
-  });
+  if (loading) return <h1>Countries loading...</h1>;
+
+  function filterCountries() {
+    return countries.filter((country) => {
+      return (
+        country.name.includes(query) && (country.continent === continent || continent === 'All')
+      );
+    });
+  }
 
   return (
       
-      <div className="countries">
-  <h1>Countries of the World</h1>
-  <input placeholder="Search Countries"
-    value={query}
-    onChange={(e) => setQuery(e.target.value)} />
-    <select value={continent} onChange={(e) => setContinent(e.target.value)}>
-<option value="All">All</option>
-<option value="Africa">Africa</option>
-<option value="Antarctica">Antarctica</option>
-<option value="Asia">Asia</option>
-<option value="Europe">Europe</option>
-<option value="North America">North America</option>
-<option value="Oceania">Oceania</option>
-<option value="South America">South America</option>
-</select>
-{filterCountries().map((country) => (
-    <div key={country.id}>
-        <img src= {`https://flagcdn.com/16x12/${country.iso2.toLowerCase()}.png`} alt={'Flag of' + country.name} />
-        <span> {'' + country.name}</span>
-</div>
-))}
-</div>
+    <div className="countries">
+      <h1>Countries of the World</h1>
+      <input placeholder="Search Countries"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} />
+      <select value={continent} onChange={(e) => setContinent(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Africa">Africa</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="Oceania">Oceania</option>
+        <option value="South America">South America</option>
+      </select>
+      {filterCountries().map((country) => (
+        <div key={country.id}>
+          <img src= {`https://flagcdn.com/16x12/${country.iso2.toLowerCase()}.png`} alt={'Flag of' + country.name} />
+          <span> {'' + country.name}</span>
+        </div>
+      ))}
+    </div>
   );
 }
-}
+
 
 
 export default useCountries;
